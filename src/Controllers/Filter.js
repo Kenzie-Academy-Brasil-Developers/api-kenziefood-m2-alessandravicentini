e8d98bb7cef89317ec9de946d00ae4525731960b
diegosales30 <diegofelipesales23@gmail.com>
2022-03-23 16:30:52 -0300

    add css no botao

--- src/Controllers/Filter.js
+++ src/Controllers/Filter.js
@@ -20,6 +20,17 @@ export class Filter {
     }
 
     static listAll(){
+        const btnAll  = document.querySelector('.btn-todos')
+        btnAll.classList.add('hover')
+
+        const btPanificadora  = document.querySelector('.btn-panificadora')
+        btPanificadora.classList.remove('hover')
+
+        const btnFruit  = document.querySelector('.btn-frutas')
+        btnFruit.classList.remove('hover')
+
+        const btnDrink  = document.querySelector('.btn-bebidas')
+        btnDrink.classList.remove('hover')
 
         const data     = JSON.parse(localStorage.getItem('products'))
         Vitrine.listarVitrine(data)
@@ -28,7 +39,7 @@ export class Filter {
 
     static listBakery(event){
         event.preventDefault()
-
+        
         const target  = event.target.name
 
         const data     = JSON.parse(localStorage.getItem('products'))
@@ -39,11 +50,34 @@ export class Filter {
                 return current.categoria
             }
         })
+        
         Vitrine.listarVitrine(output)
+        const btnAll  = document.querySelector('.btn-todos')
+        btnAll.classList.remove('hover')
+
+        const btPanificadora  = document.querySelector('.btn-panificadora')
+        btPanificadora.classList.add('hover')
+
+        const btnFruit  = document.querySelector('.btn-frutas')
+        btnFruit.classList.remove('hover')
+
+        const btnDrink  = document.querySelector('.btn-bebidas')
+        btnDrink.classList.remove('hover')
     }
 
     static listFruit(event){
         event.preventDefault()
+        const btnFruit  = document.querySelector('.btn-frutas')
+        btnFruit.classList.add('hover')
+
+        const btnAll  = document.querySelector('.btn-todos')
+        btnAll.classList.remove('hover')
+
+        const btPanificadora  = document.querySelector('.btn-panificadora')
+        btPanificadora.classList.remove('hover')
+
+        const btnDrink  = document.querySelector('.btn-bebidas')
+        btnDrink.classList.remove('hover')
 
         const target  = event.target.name
 
@@ -61,6 +95,17 @@ export class Filter {
     static listDrink(event){
         event.preventDefault()
 
+        const btnAll  = document.querySelector('.btn-todos')
+        btnAll.classList.remove('hover')
+
+        const btPanificadora  = document.querySelector('.btn-panificadora')
+        btPanificadora.classList.remove('hover')
+
+        const btnFruit  = document.querySelector('.btn-frutas')
+        btnFruit.classList.remove('hover')
+
+        const btnDrink  = document.querySelector('.btn-bebidas')
+        btnDrink.classList.add('hover')
         const target  = event.target.name
 
         const data     = JSON.parse(localStorage.getItem('products'))
